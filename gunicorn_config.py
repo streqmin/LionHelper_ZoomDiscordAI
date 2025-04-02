e3caf5737f818a8f68e21061054d4e68c678677b
changyoungOH <dhckddud1000@gmail.com>
2025-04-02 11:17:04 +0900

    fix: Render PORT 환경 변수 사용하도록 gunicorn 설정 수정

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,8 @@
 import os
 
 workers = 4
-bind = "0.0.0.0:10000"
+port = int(os.environ.get("PORT", 8000))
+bind = f"0.0.0.0:{port}"
 timeout = 300
 worker_class = "gevent"
 worker_connections = 1000
